d72b4077c4dfbd081cf5b206498c4a73644a68f5
Ayush Chauhan <ayushc262@gmail.com>
2022-12-03 21:07:58 +0530

    Brick

---
+++ BreakOut/Assets/Scripts/Brick.cs
@@ -0,0 +1,39 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Brick : MonoBehaviour
+{
+    public int hits = 1;
+    public int points = 100;
+    Rigidbody _rb;
+    BoxCollider _bc;
+
+    void Start()
+    {
+        _rb = GetComponent<Rigidbody>();
+    }
+
+    
+    void Update()
+    {
+        
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        hits--;
+        //Score
+        if(hits<=0)
+        {
+            gameObject.GetComponent<BoxCollider>().enabled = false;
+            _rb.velocity = Vector3.down * 5;
+            transform.position = transform.position + new Vector3(0, 0, -2);
+            if (transform.position.y <= -20)
+            {
+                Destroy(gameObject);
+            }
+        }
+    }
+}
