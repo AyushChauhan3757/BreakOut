4ce26694e2904c8d8b0510a241e6b65db9dfdb90
Ayush Chauhan <ayushc262@gmail.com>
2022-12-04 12:47:03 +0530

    Progress

--- BreakOut/Assets/Scripts/Brick.cs
+++ BreakOut/Assets/Scripts/Brick.cs
@@ -1,38 +1,42 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Threading.Tasks;
 using UnityEngine;
 
 public class Brick : MonoBehaviour
 {
     public int hits = 1;
     public int points = 100;
-    Rigidbody _rb;
-    BoxCollider _bc;
+    //Rigidbody _rb;
+    public Vector3 rotator;
 
     void Start()
     {
-        _rb = GetComponent<Rigidbody>();
+        //_rb = GetComponent<Rigidbody>();
     }
 
     
     void Update()
     {
-        
+        transform.Rotate(rotator * Time.deltaTime);
     }
 
-    private void OnCollisionEnter(Collision collision)
+    private async void OnCollisionEnter(Collision collision)
     {
-        hits--;
+        if (collision.gameObject.name == "Ball")
+        {
+            hits--;
+        }
         //Score
         if(hits<=0)
         {
             gameObject.GetComponent<BoxCollider>().enabled = false;
-            _rb.velocity = Vector3.down * 5;
-            if (transform.position.y <= -20)
-            {
-                Destroy(gameObject);
-            }
+            transform.position += Vector3.down *Time.deltaTime * 50;
+            await Task.Delay(800);
+            Destroy(gameObject);
+            
+            
         }
     }
 }
