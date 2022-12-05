084e98aa64f5ab27bf7f2ace648620343c97fdbb
Ayush Chauhan <ayushc262@gmail.com>
2022-12-05 21:26:31 +0530

    Menu

---
+++ BreakOut/Assets/Scripts/GameManager.cs
@@ -0,0 +1,91 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameManager : MonoBehaviour
+{
+    public GameObject BallPrefab;
+    public GameObject PlayerPrefab;
+    public Text scoreText;
+    public Text ballsText;
+    public Text levelText;
+
+    public GameObject panelMenu;
+    public GameObject panelPlay;
+    public GameObject panelLevelCcomplated;
+    public GameObject panelGameOver;
+
+    public enum State { MENU, INIT, PLAY, LEVELCOMPLETED, LOADLEVEL, GAMEOVER }
+    State _state;
+
+    void Start()
+    {
+        SwitchState(State.MENU);
+    }
+
+    public void SwitchState(State newState)
+    {
+        EndState();
+        BeginState(newState);
+    }
+
+    void BeginState(State newState)
+    {
+        switch (newState)
+        {
+            case State.MENU:
+                panelMenu.SetActive(true);
+                break;
+            case State.INIT:
+                break;
+            case State.PLAY:
+                break;
+            case State.LEVELCOMPLETED:
+                break;
+            case State.LOADLEVEL:
+                break;
+            case State.GAMEOVER:
+                break;
+        }
+    }
+
+    void Update()
+    {
+        switch (_state)
+        {
+            case State.MENU:
+                break;
+            case State.INIT:
+                break;
+            case State.PLAY:
+                break;
+            case State.LEVELCOMPLETED:
+                break;
+            case State.LOADLEVEL:
+                break;
+            case State.GAMEOVER:
+                break;
+        }
+    }
+
+    void EndState()
+    {
+        switch (_state)
+        {
+            case State.MENU:
+                panelMenu.SetActive(false);
+                break;
+            case State.INIT:
+                break;
+            case State.PLAY:
+                break;
+            case State.LEVELCOMPLETED:
+                break;
+            case State.LOADLEVEL:
+                break;
+            case State.GAMEOVER:
+                break;
+        }
+    }
+}
